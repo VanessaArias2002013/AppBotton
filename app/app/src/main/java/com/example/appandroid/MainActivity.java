9641330e07e3218ac0fba98117c580f63320aaf6
VanessaArias2002013 <2002013@utrivieramaya.edu.mx>
2022-02-02 21:32:45 -0500

    proyecto

---
+++ app/app/src/main/java/com/example/appandroid/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.appandroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"This is a message!!!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+}
+
+}
+
+
+
+
